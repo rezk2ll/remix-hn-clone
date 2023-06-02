e5469c58f49afa3aa91fe09c80cb8f017fe8fc09
Khaled FERJANI <kferjani@linagora.com>
2023-06-02 21:53:27 +0100

    added shows page

---
+++ app/routes/show.tsx
@@ -0,0 +1,18 @@
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import StoryList from '~/components/story/StoryList';
+import apiService from '~/lib/api';
+
+export async function loader() {
+  return json(await apiService.fetchShowStories());
+}
+
+export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div className='bg-hn-bg min-h-screen pt-2'>
+      <StoryList items={data} />
+    </div>
+  );
+}
