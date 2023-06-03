bf2d070d01d4c56c41afb63c76572a260c3d1f40
Khaled FERJANI <kferjani@linagora.com>
2023-06-03 18:20:59 +0100

    added utils

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,5 @@
+import type { AdjacentComments, FullItem } from '~/types';
+
 /**
  * Extracts the domain name from a URL
  *
@@ -36,4 +38,37 @@ export const getTimeDifference = (unixTimestamp: number): string => {
   }
 
   return `${seconds} seconds`;
+};
+
+/**
+ * Recursively calculates the size of a comment tree
+ *
+ * @param {FullItem} comments - the initial comment list
+ * @returns {number} the comment tree size
+ */
+export const getCommentsTreeSize = (comments: FullItem[]): number => {
+  const initialSize = comments.length;
+
+  return comments.reduce(
+    (acc, comment) => acc + getCommentsTreeSize(comment.descendants),
+    initialSize
+  );
+};
+
+/**
+ * Searches for the next and previous comment ids in a list of comments
+ *
+ * @param {FullItem[]} list the list of comments to search within
+ * @param {number} index the current comment index
+ * @returns {AdjacentComments} the previous and next comment ids
+ * @example {next: 3, previous: 1}
+ */
+export const getAdjacentComments = (
+  list: FullItem[],
+  index: number
+): AdjacentComments => {
+  return {
+    next: index === list.length - 1 ? null : list[index + 1].item.id,
+    prev: index === 0 ? null : list[index - 1].item.id,
+  };
 };
