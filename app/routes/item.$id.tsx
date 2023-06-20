5d225726c879ed489f9c8fa5ace0946bc036227d
Khaled FERJANI <kferjani@linagora.com>
2023-06-20 22:52:01 +0100

    added remix content streaming using await

--- app/routes/item.$id.tsx
+++ app/routes/item.$id.tsx
@@ -1,11 +1,14 @@
-import { json } from '@remix-run/node';
+import { defer } from '@remix-run/node';
 import type { V2_MetaFunction } from '@remix-run/node';
 import type { LoaderArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Await, useLoaderData } from '@remix-run/react';
+import { Suspense } from 'react';
+import SekeletonComments from '~/components/comment/Skeleton';
 import CommentForm from '~/components/forms/CommentForm';
 import StoryCommentTree from '~/components/story/StoryCommentTree';
 import StoryItem from '~/components/story/StoryItem';
 import apiService from '~/lib/api';
+import type { Item as ItemType } from '~/types';
 import { getAdjacentComments } from '~/utils/comment';
 
 export async function loader({ params }: LoaderArgs) {
@@ -15,28 +18,42 @@ export async function loader({ params }: LoaderArgs) {
     throw new Error('Invalid item');
   }
 
-  return json(await apiService.fetchFullItem(+id));
+  return defer({
+    item: await apiService.fetchItem<ItemType>(+id),
+    data: apiService.fetchFullItem(+id),
+  });
 }
 
-export const meta: V2_MetaFunction = ({ data }) => {
+export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
   return [{ title: data.item.title }];
 };
 
 export default function Item() {
-  const { item, descendants } = useLoaderData<typeof loader>();
+  const { data, item } = useLoaderData<typeof loader>();
 
   return (
     <div className='flex flex-col space-y-1 pt-4 bg-hn-bg dark:bg-slate-800 px-2'>
       <StoryItem item={item} />
       <CommentForm />
-      {descendants.map((comment, index) => (
-        <StoryCommentTree
-          comment={comment.item}
-          key={comment.item.id}
-          kids={comment.descendants}
-          nav={{ ...getAdjacentComments(descendants, index), parent: null }}
-        />
-      ))}
+      <Suspense fallback={<SekeletonComments />}>
+        <Await resolve={data} errorElement={<p>error</p>}>
+          {({ descendants }) => (
+            <>
+              {descendants.map((comment, index) => (
+                <StoryCommentTree
+                  comment={comment.item}
+                  key={comment.item.id}
+                  kids={comment.descendants}
+                  nav={{
+                    ...getAdjacentComments(descendants, index),
+                    parent: null,
+                  }}
+                />
+              ))}
+            </>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 }
